d2b0fffe40733f5a2a9e943b962b7515ba1450c6
NyamumboC <charlesnyamumbo@gmail.com>
2022-12-16 00:21:15 +0300

    style Book Policy Page

--- yugas/pages/index.tsx
+++ yugas/pages/index.tsx
@@ -7,7 +7,6 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
 import { useState, useEffect } from 'react';
 
-
 // Import abi
 import abi from "../utils/YugasInsurance.json";
 
@@ -16,7 +15,6 @@ declare global {
     ethereum?: any;
   }
 }
-
 
 const Home: NextPage = () => {
 
@@ -142,9 +140,9 @@ const Home: NextPage = () => {
           brand ? brand : handleOnBrandChange,
           model ? model : handleOnModelChange,
           regNo ? regNo : handleOnRegNoChange,
-          year ? year : "2",
-          chassis ? chassis : "2",
-          price ? price : "1",
+          year ? year : "2023",
+          chassis ? chassis : "26963036",
+          price ? price : "450000",
           
           // ethers.utils.parseEther("0.001"),
           // {
@@ -258,7 +256,7 @@ const Home: NextPage = () => {
           regNo: regNo,
           premium: parseInt(premium),
           riskId: riskId,
-          applicationId: applicationId,
+          applicationId: parseInt(applicationId),
         },
       ]);
     };
@@ -309,115 +307,155 @@ const Home: NextPage = () => {
 
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-slate-500">
       <Head>
         <title>Yugasl Insurance</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
+      <main className="w-full flex-1 items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold sticky top-3 z-50">
           Yugasl Insurance
         </h1>
-
+        <br />
+        
         {currentAccount ? (
-          <div className="w-full max-w-xs sticky top-3 z-50 ">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="brand">
-                  Brand
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="brand" type="text" placeholder="Brand" onChange={handleOnBrandChange} required />
-              </div>
+          <div className='columns-2'>
+            <div className="flex justify-center w-full max-w-xs sticky top-3 z-50 ">
+              <form className="bg-slate-100 shadow-md rounded px-12 pt-6">
+                <div className="mb-3">
+                  <label className="block text-gray-700 text-sm font-bold" htmlFor="brand">
+                    Brand
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="brand" type="text" placeholder="Brand" onChange={handleOnBrandChange} required />
+                </div>
 
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="model">
-                  Model
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="model" type="text" placeholder="Model" onChange={handleOnModelChange} required />
-              </div>
-              
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="regno">
-                  Registration Number
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="regno" type="text" placeholder="Reg No" onChange={handleOnRegNoChange} required />
-              </div>
+                <div className="mb-3">
+                  <label className="block text-gray-700 text-sm font-bold" htmlFor="model">
+                    Model
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="model" type="text" placeholder="Model" onChange={handleOnModelChange} required />
+                </div>
+                
+                <div className="mb-3">
+                  <label className="block text-gray-700 text-sm font-bold" htmlFor="regno">
+                    Registration Number
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="regno" type="text" placeholder="Reg No" onChange={handleOnRegNoChange} required />
+                </div>
 
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="year">
-                  Year of Manufacture
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="year" type="number" placeholder="Year of Man" onChange={handleOnYearChange} required />
-              </div>
+                <div className="mb-3">
+                  <label className="block text-gray-700 text-sm font-bold" htmlFor="year">
+                    Year of Manufacture
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="year" type="number" placeholder="Year of Man" onChange={handleOnYearChange} required />
+                </div>
 
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="chassis">
-                  Chassis Number
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="chassis" type="number" placeholder="Chassis Number" onChange={handleOnChassisChange} required />
-              </div>
+                <div className="mb-3">
+                  <label className="block text-gray-700 text-sm font-bold" htmlFor="chassis">
+                    Chassis Number
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="chassis" type="number" placeholder="Chassis Number" onChange={handleOnChassisChange} required />
+                </div>
 
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="price">
-                  Value of Vehicle
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="price" type="number" placeholder="Value of Vehicle" onChange={handleOnYearChange} required />
-              </div>
+                <div className="mb-3">
+                  <label className="block text-gray-700 text-sm font-bold" htmlFor="price">
+                    Value of Vehicle
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="price" type="number" placeholder="Value of Vehicle" onChange={handleOnYearChange} required />
+                </div>
 
+                <div className="flex items-center justify-center">
+                  <button
+                    className="bg-cyan-600 hover:bg-cyan-700 text-center text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="button"
+                    onClick={applyForPolicy}
+                  >
+                    Buy Insurance
+                  </button>
+                </div>
+                <br />
+              </form>
+            </div>
+            <div className='border'>
+              <br />
+            <p>
+              <br /><br /><br />
+                Book Your vehicle insurance with Yugasl.
+                
+                <br /> <br />
+                Pay with Matic and let us insure your vehicle
+                <br /> <br />
 
-              <div className="flex items-left justify-between">
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-center text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="button"
-                  onClick={applyForPolicy}
-                >
-                  Buy Insurance
-                </button>
-              </div>
-            </form>
+                Easy, Convenient, Fast
+                <br /> <br /> 
+                We provide motor vehicle insurance for all types of vehicles. 
+                <br /> Just fill in your vehicle details on the form provided.
+                <br /> <br /> 
+                We provide coverage for up to Kes 20 million and cover most insured damage claims related solely from injuries and damage caused by an accident.
+               
+              </p>
+              <br /><br /> <br />
+              <p>In short... Be safe. Get covered
+                <br />
+                By Yugasl
+                <br /><br /><br />
+              </p>
+  
+              <br />
+              <p></p>
+            </div>
+            <br /><br />
           </div>
         ) : (
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded-full mt-3"
-            onClick={connectWallet}
-          >
-            Connect Your Wallet
-          </button>
+          <div>
+            <div>
+              <p>
+              <br /> Providing motor vehicle insurance for all major types of vehicles, ATVs (all-terrain vehicles), golf carts or any similar type.
+              <br /> Providing coverage up to Kes 20 million as part which is limited liability with respect thereto covering most insured damage claims related solely from injuries caused by accident against such passenger car/truck while it was owned by another driver under 
+              contract on terms having been specifically arranged not exceeding six months prior at the time where collision occurred upon condition that both parties be identified individually 
+              pursuant also being licensed drivers before issuance of notice thereof unless otherwise authorize
+              </p>
+              <br />
+              <p>In short... Be safe. Get covered</p>
+              <br />
+              <p></p>
+            </div>
+            <button
+              className="bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-3 rounded-full mt-3"
+              onClick={connectWallet}
+            >
+              Get Started
+            </button>
+          </div>
         )}
 
         {allPolicy.map((policy, index) => {
           return (
-            <div className="border-l-2 mt-10" key={index}>
-              <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-blue-800 text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
-                {/* <!-- Dot Following the Left Vertical Line --> */}
-                <div className="w-5 h-5 bg-blue-600 absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0"></div>
-
-                {/* <!-- Line that connecting the box with the vertical line --> */}
-                <div className="w-10 h-1 bg-green-300 absolute -left-10 z-0"></div>
-
+            <div className="border-l-2 flex justify-center" key={index}>
+              <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 py-4 bg-cyan-700 text-white rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
                 {/* <!-- Content that showing in the box --> */}
                 <div className="flex-auto">
-                  <h1 className="text-md">Reg No: {policy['regNo']}</h1>
-                  <h1 className="text-md">Premium: {policy['premium']}</h1>
-                  <h4 className="text-md">Risk Id: {policy['riskId']}</h4>
-                  <h1 className="text-md">Application Id: {policy['applicationId']}</h1>
-                  <h3>Address: {policy['address']}</h3>
-                  <h1 className="text-md font-bold">
+                  <h3 className="text-md">Reg No: {policy['regNo']}</h3>
+                  <h4 className="text-md">Premium: {policy['premium']}</h4>
+                  <h6 className="text-md">Risk Id: {policy['riskId']}</h6>
+                  <h3 className="text-md">Application Id: {policy['applicationId']}</h3>
+                  <h4>Address: {policy['address']}</h4>
+                  <h6 className="text-md font-bold">
                     TimeStamp: {policy['timestamp'].toString()}
-                  </h1>
+                  </h6>
                 </div>
               </div>
             </div>
